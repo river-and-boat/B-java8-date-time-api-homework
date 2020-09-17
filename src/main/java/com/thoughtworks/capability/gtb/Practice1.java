4842d5337b2ddc0fa4a1545a11dea682f2a1cacc
RiverAndBoat <842714673@qq.com>
2020-09-17 17:01:18 +0800

    [Jiang Yuzhou] feat: finish exercise 1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -10,7 +11,15 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    private static final Month MONTH_OF_LABOR_DAY = Month.MAY;
+    private static final int DAY_OF_LABOR_DAY = 1;
+    private static final int OFFSET_YEAR = 1;
+
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        LocalDate laborDayOfThisYear = LocalDate.of(date.getYear(), MONTH_OF_LABOR_DAY, DAY_OF_LABOR_DAY);
+        if (date.isAfter(laborDayOfThisYear)) {
+            laborDayOfThisYear = laborDayOfThisYear.plusYears(OFFSET_YEAR);
+        }
+        return (laborDayOfThisYear.toEpochDay() - date.toEpochDay());
+    }
 }
